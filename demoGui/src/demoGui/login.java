5c04a172176f9621fed376de922c3c331fa42470
Lenovo <Lenovo@LAPTOP-TERA.home-life.hub>
2020-12-13 20:51:20 +0100

    Tolto main da Login

--- demoGui/src/demoGui/login.java
+++ demoGui/src/demoGui/login.java
@@ -17,84 +17,90 @@ import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JFormattedTextField;
 import java.awt.CardLayout;
+import java.awt.SystemColor;
+import java.awt.Window.Type;
+import javax.swing.JPasswordField;
+import javax.swing.JSeparator;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.Dialog.ModalExclusionType;
 
 public class login extends JFrame {
 
 	private JPanel contentPane;
+	private final JPasswordField passwordField = new JPasswordField();
 
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					login frame = new login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	
 
 	/**
 	 * Create the frame.
 	 */
 	public login() {
-		setAlwaysOnTop(true);
+		setModalExclusionType(ModalExclusionType.APPLICATION_EXCLUDE);
+		setFont(new Font("Arial", Font.PLAIN, 20));
+		setType(Type.POPUP);
 		setResizable(false);
-		setBackground(Color.BLACK);
+		setAlwaysOnTop(true);
+		setBackground(UIManager.getColor("CheckBox.highlight"));
 		setTitle("LOGIN/USER");
-		setLocation(new Point(200, 400));
+		setLocation(new Point(0, 476));
 		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Lenovo\\Pictures\\Camera Roll\\albero1.jfif"));
 		setForeground(UIManager.getColor("CheckBoxMenuItem.selectionBackground"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(300, 300, 352, 362);
+		setBounds(300, 300, 352, 464);
 		contentPane = new JPanel();
-		contentPane.setBackground(Color.BLACK);
+		contentPane.setBackground(UIManager.getColor("CheckBox.highlight"));
 		contentPane.setLocation(new Point(100, 200));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lableLogin = new JLabel("LOGIN");
-		lableLogin.setBounds(68, 0, 171, 61);
+		lableLogin.setBounds(93, 10, 153, 90);
 		lableLogin.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 22));
-		lableLogin.setForeground(Color.WHITE);
+		lableLogin.setForeground(UIManager.getColor("CheckBox.foreground"));
 		lableLogin.setHorizontalAlignment(SwingConstants.CENTER);
 		lableLogin.setBackground(Color.BLACK);
 		contentPane.add(lableLogin);
 		
 		JLabel lblNome = new JLabel("Email");
-		lblNome.setBounds(10, 79, 74, 66);
-		lblNome.setHorizontalAlignment(SwingConstants.LEFT);
-		lblNome.setForeground(Color.WHITE);
+		lblNome.setBounds(10, 108, 129, 84);
+		lblNome.setAlignmentY(Component.BOTTOM_ALIGNMENT);
+		lblNome.setAlignmentX(Component.RIGHT_ALIGNMENT);
+		lblNome.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNome.setForeground(Color.BLACK);
 		lblNome.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 22));
 		lblNome.setBackground(Color.BLACK);
 		contentPane.add(lblNome);
 		
 		JLabel lblPassword = new JLabel("Password");
-		lblPassword.setBounds(10, 156, 113, 61);
-		lblPassword.setHorizontalAlignment(SwingConstants.LEFT);
-		lblPassword.setForeground(Color.WHITE);
+		lblPassword.setBounds(10, 192, 129, 84);
+		lblPassword.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPassword.setForeground(Color.BLACK);
 		lblPassword.setFont(new Font("Dialog", Font.BOLD | Font.ITALIC, 22));
 		lblPassword.setBackground(Color.BLACK);
 		contentPane.add(lblPassword);
 		
-		JFormattedTextField formattedTextFieldEmailUser = new JFormattedTextField();
-		formattedTextFieldEmailUser.setBounds(158, 91, 171, 47);
-		formattedTextFieldEmailUser.setDragEnabled(true);
-		formattedTextFieldEmailUser.setToolTipText("Inserire Email");
-		formattedTextFieldEmailUser.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
-		contentPane.add(formattedTextFieldEmailUser);
+		JFormattedTextField emaliTA = new JFormattedTextField();
+		emaliTA.setBounds(156, 128, 153, 49);
+		emaliTA.setDragEnabled(true);
+		emaliTA.setToolTipText("Inserire Email");
+		emaliTA.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
+		contentPane.add(emaliTA);
+		passwordField.setBounds(156, 215, 153, 49);
+		contentPane.add(passwordField);
 		
-		JFormattedTextField formattedTextFieldPasswordUser = new JFormattedTextField();
-		formattedTextFieldPasswordUser.setBackground(Color.CYAN);
-		formattedTextFieldPasswordUser.setToolTipText("Inserire Password");
-		formattedTextFieldPasswordUser.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 20));
-		formattedTextFieldPasswordUser.setDragEnabled(true);
-		formattedTextFieldPasswordUser.setBounds(158, 165, 171, 47);
-		contentPane.add(formattedTextFieldPasswordUser);
+		JSeparator separator = new JSeparator();
+		separator.setBounds(27, 95, 296, 27);
+		contentPane.add(separator);
+		
+		JButton btnNewButton = new JButton("commit");
+		btnNewButton.setFont(new Font("Sitka Small", Font.PLAIN, 20));
+		btnNewButton.setBounds(156, 336, 153, 50);
+		contentPane.add(btnNewButton);
 	}
 }
